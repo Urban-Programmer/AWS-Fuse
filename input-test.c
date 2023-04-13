3afaf845cb6d67ed77ae986795b8c94d5ad89dbb
Chris Wilson <awil17@outlook.com>
2023-04-12 23:09:39 -0400

    Adding basic functionality to main program

---
+++ input-test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+
+    
+    printf("Arg: %s\n", argv[1]);
+
+
+    exit(0);
+}
